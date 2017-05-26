6882145c4acd5c97901376106e6f92381a24e474
delcanovega <delcanovega@gmail.com>
2017-05-26 13:17:59 +0200

    Script for indexing mongoDB with ES

---
+++ elastic_index.py
@@ -0,0 +1,18 @@
+# This script indexes out MongoDB collection
+# with the ElasticSearch engine in order to
+# perform more optimal searches
+
+from elasticsearch import Elasticsearch
+from humbledb import Mongo, Document
+
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+class MovieDoc(Document):
+    config_database = 'emdb'
+    config_collection = 'movies'
+
+with Mongo:
+    for m in MovieDoc.find():
+        content = m.for_json()
+        content.pop('_id', None)
+        es.index(index='emdb', doc_type='movie', id=m['_id'], body=content)
